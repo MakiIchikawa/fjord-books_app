b978aec830d9bdc64cf0ad7a64a357fadc3535e8
MakiIchikawa <punkfolkfolk@gmail.com>
2021-01-02 11:02:42 +0900

    ログイン、ログアウト後のリダイレクト先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  def after_sign_in_path_for(_resource)
+    if current_user
+      flash[:notice] = 'ログインに成功しました'
+      books_path
+    else
+      flash[:notice] = '新規登録完了しました'
+      books_path
+    end
+  end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
