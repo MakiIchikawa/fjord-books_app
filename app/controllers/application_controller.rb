6d59493f9538832f82d92dc6f6202c08facb2421
MakiIchikawa <punkfolkfolk@gmail.com>
2021-01-21 22:19:52 +0900

    不要なコードは削除し、deviseのデフォルトの機能に委ねる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,10 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(_resource)
-    flash[:notice] = if current_user
-                       'ログインに成功しました'
-                     else
-                       '新規登録完了しました'
-                     end
     books_path
   end
 
   def after_sign_out_path_for(_resource)
-    flash[:notice] = 'ログアウトしました'
     new_user_session_path
   end
 end
