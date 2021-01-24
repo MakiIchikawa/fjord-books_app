cf726f964d6619a397f85c2e5e46dcfab83419b7
MakiIchikawa <punkfolkfolk@gmail.com>
2021-01-24 22:45:48 +0900

    RuboCopに伴う修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
