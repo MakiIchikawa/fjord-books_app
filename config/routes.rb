b58724666d34b8f6802955e7cb62925b284187b2
MakiIchikawa <punkfolkfolk@gmail.com>
2021-02-04 18:30:00 +0900

    githubコールバック後のルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
+  devise_scope :user do
+    get 'users/auth/github/callback' => 'users/omniauth_callbacks#github'
+  end
   resources :books
   resources :users, only: %i(index show)
 end
