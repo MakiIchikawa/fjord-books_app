eed43b77832002caaa44381e4271ba0737b1a345
MakiIchikawa <punkfolkfolk@gmail.com>
2021-02-04 18:30:41 +0900

    不要なコードの削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    byebug
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
