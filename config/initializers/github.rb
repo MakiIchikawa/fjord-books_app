6fe68d9eda3bd0bf674730c22a69d0f240a8f600
MakiIchikawa <punkfolkfolk@gmail.com>
2021-02-16 10:17:28 +0900

    正式名称に変更

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GIT_OAUTH_ID'], ENV['GIT_OAUTH_SECRET']
+  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
 end
