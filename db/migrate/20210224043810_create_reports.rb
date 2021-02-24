8c565547ce6733f0086072478ca8404252849e79
MakiIchikawa <punkfolkfolk@gmail.com>
2021-02-24 13:55:13 +0900

    レポートテーブルの作成

--- db/migrate/20210224043810_create_reports.rb
+++ db/migrate/20210224043810_create_reports.rb
@@ -3,7 +3,7 @@ class CreateReports < ActiveRecord::Migration[6.1]
     create_table :reports do |t|
       t.text :title
       t.text :content
-      t.references :created_by, null: false, foreign_key: true
+      t.references :created_by, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_052521) do
+ActiveRecord::Schema.define(version: 2021_02_24_043810) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,15 @@ ActiveRecord::Schema.define(version: 2020_12_27_052521) do
     t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.integer "created_by_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["created_by_id"], name: "index_reports_on_created_by_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,4 +88,5 @@ ActiveRecord::Schema.define(version: 2020_12_27_052521) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reports", "users", column: "created_by_id"
 end
