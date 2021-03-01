f5937232ebba8c1a39818466d21d4bbfaa252a57
MakiIchikawa <punkfolkfolk@gmail.com>
2021-03-01 13:57:02 +0900

    不要なコメント、コードを削除

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,25 +2,17 @@
 
 class RelationshipsController < ApplicationController
   before_action :set_user, only: %i[create destroy]
-  # POST /books
-  # POST /books.json
   def create
-    respond_to do |format|
-      if current_user.active_relationships.create!(followed_id: @user.id)
-        format.html { redirect_to @user, notice: t('controllers.relationship.notice_follow') }
-      else
-        format.html { redirect_to @user }
-      end
+    if current_user.active_relationships.create!(followed_id: @user.id)
+      redirect_to @user, notice: t('controllers.relationship.notice_follow')
+    else
+      redirect_to @user
     end
   end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     current_user.active_relationships.find_by(followed_id: @user.id).destroy
-    respond_to do |format|
-      format.html { redirect_to @user, notice: t('controllers.relationship.notice_unfollow') }
-    end
+    redirect_to @user, notice: t('controllers.relationship.notice_unfollow')
   end
 
   private
