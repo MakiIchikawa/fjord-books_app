cbc5e8a8a2e83756919bf9bbc15b601f59f5393f
MakiIchikawa <punkfolkfolk@gmail.com>
2021-03-01 16:41:04 +0900

    n+1問題回避のため、コントローラ側でavatarのincludeを行う

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -4,6 +4,6 @@ class FollowersController < RelationshipsController
   before_action :set_user, only: %i[show]
 
   def show
-    @followers = @user.followers.order(:id).page(params[:page])
+    @followers = @user.followers.order(:id).page(params[:page]).includes(avatar_attachment: :blob)
   end
 end
--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -4,6 +4,6 @@ class FollowingsController < RelationshipsController
   before_action :set_user, only: %i[show]
 
   def show
-    @followings = @user.followings.order(:id).page(params[:page])
+    @followings = @user.followings.order(:id).page(params[:page]).include(avatar_attachment: :blob)
   end
 end
