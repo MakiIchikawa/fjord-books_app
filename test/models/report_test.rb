c3dbf367ffd1e91fde408639faa25a8fb1340bec
MakiIchikawa <punkfolkfolk@gmail.com>
2021-03-06 20:35:28 +0900

    レポートモデルのモデルテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @viewer = User.create!(email: 'viewer@example.com', password: 'password')
+    @writer = User.create!(email: 'writer@example.com', password: 'password')
+    @report = @writer.reports.new(title: 'hoge', content: 'hogehoge')
+  end
+
+  test 'editable?メソッドの検証' do
+    assert @report.editable?(@writer)
+    assert_not @report.editable?(@viewer)
+  end
+
+  test 'created_onメソッドの検証' do
+    @report.save!
+    assert_equal Time.zone.today, @report.created_on
+  end
 end
