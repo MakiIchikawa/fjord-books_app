125ed2673b453c3cbf8fab661409a423ce349b91
MakiIchikawa <punkfolkfolk@gmail.com>
2021-03-06 19:27:53 +0900

    ユーザモデルのモデルテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo Bar'
+    assert_equal 'Foo Bar', user.name_or_email
+  end
+
+  setup do
+    @me = User.create!(email: 'me@example.com', password: 'password')
+    @she = User.create!(email: 'she@example.com', password: 'password')
+  end
+
+  test 'follow/following?メソッドを検証する' do
+    assert_not @me.following?(@she)
+    @me.follow(@she)
+    assert @me.following?(@she)
+  end
+
+  test 'unfollow/followed_by?メソッドを検証する' do
+    @me.follow(@she)
+    assert @she.followed_by?(@me)
+    @me.unfollow(@she)
+    assert_not @she.followed_by?(@me)
+  end
 end
